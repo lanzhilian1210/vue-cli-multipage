3ededb71bf247dbfa09cf71f2309f87566542533
weichenyu <weichenyu@ihsmf.com>
2018-11-27 14:00:20 +0800

    vue多页面

---
+++ src/components/detail/detail.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        detailsssssss
+    </div>
+</template>
---
+++ src/module/detail/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./images/logo.png">
+    <div>详情内容</div>
+    <detail></detail>
+  </div>
+</template>
+
+<script>
+  import 'common/css/reset.css';
+  import detail from 'components/detail/detail'
+
+  export default {
+    name: 'app',
+    components: {
+      detail
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
